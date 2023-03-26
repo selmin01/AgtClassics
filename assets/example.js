c9bc7d53b0477fc52d60550a833e77b69c9fae4f
selmin01 <anselmogabriel421@gmail.com>
2023-03-26 18:21:22 -0300

    Ajustes top hahah

--- assets/example.js
+++ assets/example.js
@@ -1,5 +1,3 @@
-//Author: Rajeev Lochan Pandey
-
 //Variables
 var input=document.getElementById("input");
 var output=document.getElementById("output");
@@ -7,9 +5,6 @@ var submit=document.getElementById("submit");
 var valueInput;
 var sortedArray=[];
 var result='';
-
-
-
 
 //Events
 input.onfocus=function(){
@@ -26,28 +21,83 @@ submit.onclick=function(){
 function grabInput(){
     valueInput=document.getElementById("input").value;
     valueInput=valueInput.split(",");
-    giveInput();
-}
-
-function giveInput(){
-    for(i=0;i<valueInput.length;i++){
-        console.log(i);
-        sortedArray.push(valueInput[i]);
-        sortFunction(i);
-    }
+    // sortedArray = insSort(valueInput);
+    // sortedArray = bubleShort(valueInput);
+    sortedArray = quickSort(valueInput);
     concatenate();
+    //giveInput();
 }
 
-function sortFunction(i){
-    if(i!=0){
-        while(i!=0 && sortedArray[i-1]>sortedArray[i]){
-            var dummy=sortedArray[i-1];
-            sortedArray[i-1]=sortedArray[i];
-            sortedArray[i]=dummy;
-            i--;
+function insSort(arr){
+    let position,value;
+
+    for (let i=0;i<arr.length;i++){
+        value=arr[i]; // pegando elementos[i] para comparar
+        position=i;   
+        while(position>0 && arr[position-1]>value){ // comparando
+            arr[position]=arr[position-1]; // troca
+            position=position-1;           // verifica se há elementos antes dele
+        }
+        arr[position]=value; // substitui valor
+    }
+    
+  return arr;
+}
+
+function bubleShort(arr){
+    for(let i=0; i<arr.length; i++){
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[i] > arr[j]){
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp
+            }
         }
     }
+
+    return arr;
 }
+
+function quickSort(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+  
+    const pivot = arr[arr.length - 1];
+    const left = [];
+    const right = [];
+  
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+  
+    return [...quickSort(left), pivot, ...quickSort(right)];
+  }
+
+
+// function giveInput(){
+//     for(i=0;i<valueInput.length;i++){
+//         console.log(i);
+//         sortedArray.push(valueInput[i]);
+//         sortFunction(i);
+//     }
+//     concatenate();
+// }
+
+// function sortFunction(i){
+//     if(i!=0){
+//         while(i!=0 && sortedArray[i-1]>sortedArray[i]){
+//             var dummy=sortedArray[i-1];
+//             sortedArray[i-1]=sortedArray[i];
+//             sortedArray[i]=dummy;
+//             i--;
+//         }
+//     }
+// }
 
 function concatenate(){
     for(i=0;i<sortedArray.length;i++){
